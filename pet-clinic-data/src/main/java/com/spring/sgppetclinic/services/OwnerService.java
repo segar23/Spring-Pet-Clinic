8fea1ee7725382a199cb90f9bdc1bdc83968f4d3
Sebastian Garces Palacio <segarces@gmail.com>
2019-07-14 13:49:23 -0400

    Refactoring to Common Interface
    Closes #11

--- pet-clinic-data/src/main/java/com/spring/sgppetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/spring/sgppetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.spring.sgppetclinic.services;
 
 import com.spring.sgppetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/spring/sgppetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/spring/sgppetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.spring.sgppetclinic.services;
 
 import com.spring.sgppetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
